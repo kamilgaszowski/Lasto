d6c54953492eaa265a13ebb8c410bbc888c5379f
kamilgaszowski <48551409+kamilgaszowski@users.noreply.github.com>
2026-01-17 21:51:32 +0100

    Add components Icon

---
+++ app/components/Icons.tsx
@@ -0,0 +1,64 @@
+
+
+export const RuneArrowLeft = () => (
+  <span className="icon-md icon-bold icon-square">
+    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor"><path d="M16 4l-10 8 10 8" /></svg>
+  </span>
+);
+
+export const RuneArrowRight = () => (
+  <span className="icon-md icon-bold icon-square">
+    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor"><path d="M8 4l10 8-10 8" /></svg>
+  </span>
+);
+
+export const SettingsIcon = () => (
+  <span className="icon-xl icon-thin">
+    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor">
+      <path d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378.138.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z" />
+      <path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+    </svg>
+  </span>
+);
+
+export const EditIcon = () => (
+  <span className="icon-md icon-normal">
+    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor"><path d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" /></svg>
+  </span>
+);
+
+export const CheckIcon = () => (
+  <span className="icon-md icon-normal">
+    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor"><path d="M4.5 12.75l6 6 9-13.5" /></svg>
+  </span>
+);
+
+export const CloseIcon = () => (
+  <span className="icon-lg icon-normal">
+    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor"><path d="M6 18L18 6M6 6l12 12" /></svg>
+  </span>
+);
+
+export const TrashIcon = () => (
+  <span className="icon-md icon-normal">
+    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor"><path d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" /></svg>
+  </span>
+);
+
+export const SunIcon = () => (
+  <span className="icon-md icon-normal">
+    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor"><path d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" /></svg>
+  </span>
+);
+
+export const MoonIcon = () => (
+  <span className="icon-md icon-normal">
+    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor"><path d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" /></svg>
+  </span>
+);
+
+export const InfoIcon = () => (
+  <span className="icon-lg icon-normal">
+    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor"><path d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" /></svg>
+  </span>
+);
