1982dba09d06cb01bed0a8ca820eb35e040ec453
kamilgaszowski <48551409+kamilgaszowski@users.noreply.github.com>
2026-01-17 23:35:34 +0100

    Add build trace and turbopack configuration files
    - Introduced a new trace-build file to log build performance metrics including durations for various build steps such as turbopack execution, TypeScript compilation, static checks, and static generation.
    - Added an empty turbopack configuration file to facilitate future enhancements and configurations.

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"vLPzoRZWq1s7LQ208el4kTAtZtV7+hrf6K84Jticfqo=\"\n}"
